083ce2d4d70fd6a13f8ea502153f8427969fb0ca
Aarav Sharma <aaravlikestennis@gmail.com>
2026-03-03 20:35:03 -0800

    updating file; ready for testing

--- binary_search.py
+++ binary_search.py
@@ -1,15 +1,15 @@
 def binary_search(arr, target):
     left = 0
-    right = len(arr)  # Bug: should be len(arr) - 1
+    right = len(arr)
     
-    while left < right:  # Bug: should be left <= right
-        mid = (left + right) / 2  # Bug: should be // for integer division
+    while left < right:
+        mid = (left + right) / 2
         
         if arr[mid] == target:
             return mid
         elif arr[mid] < target:
-            left = mid  # Bug: should be mid + 1
+            left = mid
         else:
-            right = mid  # Bug: should be mid - 1
+            right = mid
     
     return -1
