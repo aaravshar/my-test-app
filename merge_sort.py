9733a22662955abcc672716fea6a785f91a77829
Aarav Sharma <aaravlikestennis@gmail.com>
2026-03-05 21:39:33 -0800

    Initial implementation

--- merge_sort.py
+++ merge_sort.py
@@ -1,5 +1,5 @@
 def merge_sort(arr):
-    if len(arr) <= 0: 
+    if len(arr) <= 1: 
         return arr
 
     mid = len(arr) // 2
@@ -7,22 +7,25 @@ def merge_sort(arr):
     right_half = arr[mid:]
 
     # Recursive calls
-    merge_sort(left_half)
-    merge_sort(right_half)
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
 
     # Merge process
     i = j = k = 0
 
     while i < len(left_half) and j < len(right_half):
-        if left_half[i] < right_half[j]:
+        if left_half[i] <= right_half[j]:
             arr[k] = left_half[i]
             i += 1
         else:
             arr[k] = right_half[j]
             j += 1
+        k += 1
+
     while i < len(left_half):
         arr[k] = left_half[i]
         i += 1
+        k += 1
 
     while j < len(right_half):
         arr[k] = right_half[j]
