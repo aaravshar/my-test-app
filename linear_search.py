552338859037902b2a566f2ba8400254a006b407
Aarav Sharma <aaravlikestennis@gmail.com>
2026-03-05 22:25:28 -0800

    Initial implementation

--- linear_search.py
+++ linear_search.py
@@ -1,6 +1,6 @@
 def linear_search(arr, target):
     """Search for target in a list. Returns the index if found, -1 otherwise."""
-    for i in range(len(arr) - 1):  # BUG: should be range(len(arr))
+    for i in range(len(arr)):  # FIXED: changed from range(len(arr) - 1) to range(len(arr))
         if arr[i] == target:
             return i
     return -1
